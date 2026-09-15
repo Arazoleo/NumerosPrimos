d2bbe2298922c760011503e434492ff6ad789b1f
pedroabreuu <99131505+pedroabreuu@users.noreply.github.com>
2026-09-15 11:24:48 -0300

    feat: atualiza membros

--- react-app/src/data/members.js
+++ react-app/src/data/members.js
@@ -1,29 +1,37 @@
 export const members = [
   { file: 'grasi',   name: 'Grasiele',  role: 'Coordenadora', prime: 2,  isCoord: true,
-    bio: 'Coordenadora do projeto e pesquisadora em Teoria algébrica dos números e Teoria da Informação.' },
+    bio: 'Coordenadora do projeto e pesquisadora em Teoria Algébrica dos Números e Teoria da Informação.' },
   { file: 'leo',     name: 'Leonardo',  role: 'Membro',       prime: 3,
-    bio: 'Pesquisador em IA e Álgebra Linear Computacional com interesse em criptografia.' },
+    bio: 'Tech Lead do time de desenvolvimento e pesquisador em Inteligência Artificial.' },
   { file: 'abreu',   name: 'Pedro',     role: 'Membro',       prime: 5,
-    bio: 'Pesquisador em Compressão de modelos e Metaheurísticas com viés matemático.' },
-  { file: 'ana',     name: 'Ana',       role: 'Membro',       prime: 7,
-    bio: 'Engenheira de Software e professora de matemática.' },
-  { file: 'arthur',  name: 'Arthur',    role: 'Membro',       prime: 11,
-    bio: 'Desenvolvedor e admirador de conceitos sobre criptografia.' },
-  { file: 'enrique', name: 'Enrique',   role: 'Membro',       prime: 13,
-    bio: 'Pesquisador em Cyber-segurança e Machine Learning.' },
-  { file: 'flavio',  name: 'Flávio',    role: 'Membro',       prime: 17,
-    bio: 'Desenvolvedor e entusiasta de novas tecnologias' },
-  { file: 'leticia', name: 'Letícia',   role: 'Membro',       prime: 19,
+    bio: 'Membro do time de desenvolvimento e pesquisador em métodos heurísticos para Otimização Combinatória.' },
+  { file: 'ana',     name: 'Ana Beatriz',       role: 'Membro',       prime: 7,
+    bio: 'Membro do time de desenvolvimento, engenheira de software e professora de matemática.' },
+  { file: 'enrique', name: 'Enrique',   role: 'Membro',       prime: 11,
+    bio: 'Membro do time de desenvolvimento e pesquisador em Cyber-Segurança e Machine Learning.' },
+  { file: 'leticia', name: 'Letícia',   role: 'Membro',       prime: 13,
     bio: 'Pesquisa aritmética modular e suas conexões com criptografia.' },
-  { file: 'menina',  name: 'Maria Luiza', role: 'Membro',       prime: 23,
+  { file: 'marialuiza',  name: 'Maria Luiza', role: 'Membro',       prime: 17,
     bio: 'Revisora do projeto e admiradora dos números primos.' },
-  { file: 'miguel',  name: 'Miguel',    role: 'Membro',       prime: 29,
-    bio: 'Um analista de dados apaixonado por matemática.' },
-  { file: 'jose', name: 'José', role: 'Membro', prime: 31, 
+  { file: 'miguel',  name: 'Miguel',    role: 'Membro',       prime: 19,
+    bio: 'Membro do time de desenvolvimento e analista de dados apaixonado por matemática.' },
+  { file: 'jose', name: 'José', role: 'Membro', prime: 23,
     bio: 'Altamente interessado em redes de computadores e computação em nuvem.'
   },
-  { file: 'let', name: 'Letícia', role: 'Membro', prime: 37, 
-    bio: 'Pesquisadora de métodos da Computação para a análise de dados do Meio Ambiente.'
+  { file: 'igor', name: 'Igor', role: 'Membro', prime: 29,
+    bio: 'Membro do time de desenvolvimento e estudante de ciência e tecnologia.'
+  },
+  { file: 'layza', name: 'Layza', role: 'Membro', prime: 31,
+    bio: 'Membro do time de desenvolvimento e estudante de engenharia da computação.'
+  },
+  { file: 'marcos', name: 'Marcos', role: 'Membro', prime: 37,
+    bio: 'Revisor de conteúdo das publicações e estudante de Matemática Computacional.'
+  },
+  { file: 'vitor', name: 'Vítor', role: 'Membro', prime: 41,
+    bio: 'Membro do time de desenvolvimento, entusiasta da matemática e segurança da informação.'
+  },
+  { file: 'enrico', name: 'Enrico', role: 'Membro', prime: 43,
+    bio: 'Membro do time de desenvolvimento, fã de enigmas e criptografia.'
   }
 ]
 

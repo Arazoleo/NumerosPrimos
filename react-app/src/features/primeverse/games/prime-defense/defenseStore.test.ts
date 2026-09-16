c5aed14615b513f3e4e0a131a3eb4b568006e7fb
Nicolas Faria <nicolas.faria@unifesp.br>
2026-09-15 22:42:49 -0300

    Correção de erros

--- react-app/src/features/primeverse/games/prime-defense/defenseStore.test.ts
+++ react-app/src/features/primeverse/games/prime-defense/defenseStore.test.ts
@@ -7,15 +7,10 @@ import type { DefenseDivisor, DefenseLane, DefenseSlot } from './types'
 type Plan = readonly [DefenseLane, DefenseSlot, DefenseDivisor][]
 
 const PERFECT_PLANS: readonly Plan[] = [
-  // Onda 0 (Tutorial)
   [[1, 0, 2], [2, 0, 3]],
-  // Onda 1
-  [[0, 0, 7], [1, 0, 5], [2, 0, 3]],
-  // Onda 2
+  [[0, 0, 2], [1, 0, 3], [2, 0, 5]],
   [[0, 0, 2], [0, 1, 3], [1, 0, 7], [1, 1, 3], [2, 0, 2], [2, 1, 5]],
-  // Onda 3
   [[0, 0, 3], [0, 1, 5], [1, 0, 7], [2, 0, 2], [2, 1, 3]],
-  // Onda 4
   [[0, 0, 2], [0, 1, 3], [0, 2, 5], [1, 0, 7], [1, 1, 3], [1, 2, 5], [2, 0, 2], [2, 1, 3]],
 ]
 
